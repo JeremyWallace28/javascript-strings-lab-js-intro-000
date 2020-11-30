f3b8ac5ba868a68aad055a5cc570cec3cffded59
Jeremy Wallace <jwjwallace28@gmail.com>
2020-11-30 04:16:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Hello, Neil DeGrasse Tyson"
 
-var greetSpecialGuest = "Hello" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
